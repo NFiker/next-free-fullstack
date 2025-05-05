c65ff5edec5aa4ef8e139dca367a53d043596390
nasredine kherfi <nasredine.kherfi@oclock.school>
2025-05-05 13:08:56 +0200

    ✨ 03 - RSC-SERVER-DATASOURCE > get and display user from db

--- src/app/rsc-bdd/page.exercise.tsx
+++ src/app/rsc-bdd/page.exercise.tsx
@@ -1,18 +1,22 @@
 // 🐶 Dans cet exercice, nous allons apprendre à utiliser la base de données dans un Server Component
 // 🤖  Voici le code de base pour commencer
 
+import {getUserByEmail} from '@/db'
+
 //import {getUserByEmail} from '@/db'
 
-const Page = () => {
+const Page = async () => {
   // Ton code ici
   // 1. Appelle `getUserByEmail` avec l'email `user@gmail.com`
-  // 2. Affiche le nom de l'utilisateur dans le `JSX`
-  // 3. Ajoute un `console.log` pour déboguer
+  const user = await getUserByEmail('user@gmail.com')
+  // 2. Ajoute un `console.log` pour déboguer
+  console.log(user)
 
+  // 3. Affiche le nom de l'utilisateur dans le `JSX`
   return (
     <div>
       <p>Appel de la BDD</p>
-      {/* <p>user : {user?.name}</p> */}
+      <p>user : {user?.name}</p>
     </div>
   )
 }
